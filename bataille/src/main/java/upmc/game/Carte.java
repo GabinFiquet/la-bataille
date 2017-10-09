c5626d0f2bd0c9af3effb3588950f11791a3cc1e
unknown <gabin.fiquet@gmail.com>
2017-10-09 17:39:18 +0200

    add lecture console et lecture fichier (vide)

--- bataille/src/main/java/upmc/game/Carte.java
+++ bataille/src/main/java/upmc/game/Carte.java
@@ -1,63 +1,70 @@
 package upmc.game;
 
 public class Carte {
+
     /**
      * Attributs
      */
     private int valeur;
     private int couleur;
-    
-    public static String tableauCouleur[] = {" de Trèfle"," de Cœur"," de Carreau"," de Pique"};
-    public static String tableauValeur[] = {"2","3","4","5","6","7","8","9","10","Valet","Dame","Roi","1"};
-    
+
+    public static String tableauCouleur[] = {" de Trèfle", " de Cœur", " de Carreau", " de Pique"};
+    public static String tableauValeur[] = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Valet", "Dame", "Roi", "1"};
+
     /**
-     * Constructeur d'une carte 
+     * Constructeur d'une carte
      */
     public Carte(int pcouleur, int pvaleur) {
         this.valeur = pvaleur;
         this.couleur = pcouleur;
-     }
+    }
+
     /**
      * Retourne la valeur de la carte
      */
     public int getValeur() {
         return valeur;
     }
+
     /**
-     * Dédinit la valeur de la carte 
+     * Dédinit la valeur de la carte
      */
     public void setValeur(int valeur) {
         this.valeur = valeur;
     }
+
     /**
-     * Retourne la couleur de la carte 
+     * Retourne la couleur de la carte
      */
     public int getCouleur() {
         return couleur;
     }
+
     /**
      * Dédinit la couleur de la carte
      */
     public void setCouleur(int couleur) {
         this.couleur = couleur;
     }
+
     /**
-     * Retourne le résultat d'une manche 
-     * en comparant Les deux cartes jouées
+     * Retourne le résultat d'une manche en comparant Les deux cartes jouées
      */
-     public int resultatManche(Carte carteAdverse) {  
-        if(this.valeur > carteAdverse.valeur)
+    public int resultatManche(Carte carteAdverse) {
+        if (this.valeur > carteAdverse.valeur) {
             return 1;
-        else if (this.valeur < carteAdverse.valeur)
+        } else if (this.valeur < carteAdverse.valeur) {
             return -1;
-        else
+        } else {
             return 0;
+        }
     }
+
     /**
      * Retourne l'affichage de la carte
-     */ 
+     */
     public String toString() {
-        String retour = tableauValeur[valeur] + tableauCouleur[couleur] ;                
-        return  retour ;
+        String retour = tableauValeur[valeur] + tableauCouleur[couleur];
+        return retour;
     }
 }
--- bataille/src/main/java/upmc/game/Paquet.java
+++ bataille/src/main/java/upmc/game/Paquet.java
@@ -5,71 +5,72 @@ import java.util.Collections;
 import java.util.List;
 
 public class Paquet {
+
     //--
     // Attributs
     //--
     private List<Carte> tas_joueur = new ArrayList<Carte>();
-    
+
     /**
      * Constructeur faisant un deck par rapport aux parametres des cartes.
-     * Prends en parametre un boolean remplissage
-     * si faux : cree un deck vide
-     * si vrai : rempli le deck de cartes 
+     * Prends en parametre un boolean remplissage si faux : cree un deck vide si
+     * vrai : rempli le deck de cartes
      */
     public Paquet(boolean remplissage) {
-        if(remplissage==true){
-            for(int c=0; c < Carte.tableauCouleur.length; c++) {
-                for(int v=0; v < Carte.tableauValeur.length; v++ ) {
-                    Carte carteActuelle = new Carte(c,v);           
+        if (remplissage == true) {
+            for (int c = 0; c < Carte.tableauCouleur.length; c++) {
+                for (int v = 0; v < Carte.tableauValeur.length; v++) {
+                    Carte carteActuelle = new Carte(c, v);
                     tas_joueur.add(carteActuelle);
                 }
             }
         }
     }
-    
+
     /**
      * Retourne le nombre de carte
      */
     public int nbCartes() {
         return this.tas_joueur.size();
     }
-    
+
     /**
      * Retourne le paquet de cartes en entier
      */
     public String toString() {
-        String result="";
+        String result = "";
 
-        for(int i=0; i<tas_joueur.size(); i++){
-            result+=(i+1)+" : "+tas_joueur.get(i).toString()+"\n";
+        for (int i = 0; i < tas_joueur.size(); i++) {
+            result += (i + 1) + " : " + tas_joueur.get(i).toString() + "\n";
         }
         return result;
     }
-    
+
     /**
      * Retourne vrai si le deck est vide, sinon retourne faux
      */
-    public boolean estVide() {        
-        if(this.tas_joueur.size() == 0) 
+    public boolean estVide() {
+        if (this.tas_joueur.size() == 0) {
             return true;
-        else
+        } else {
             return false;
+        }
     }
-    
+
     /**
      * Melange le deck
      */
     public void melange() {
         Collections.shuffle(this.tas_joueur);
     }
-    
+
     /**
      * Retourne la dernière cartes du deck et la supprime
      */
     public Carte derniereCarte() {
-        return this.tas_joueur.remove(tas_joueur.size()-1);
+        return this.tas_joueur.remove(tas_joueur.size() - 1);
     }
-    
+
     /**
      * Ajoute une carte au debut du deck
      */
